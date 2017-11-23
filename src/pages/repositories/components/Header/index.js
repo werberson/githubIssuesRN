bc3db4ab3dd974a33b15f7a6687cbf739785e4d2
Werberson <werberson.silva@gmail.com>
2017-11-23 03:13:22 -0200

    consultando e  persistindo repositorio

--- src/pages/repositories/components/Header/index.js
+++ src/pages/repositories/components/Header/index.js
@@ -4,28 +4,29 @@ import {
   View,
   TextInput,
   TouchableOpacity,
-  AsyncStorage,
 } from 'react-native';
 
-import { NavigationActions } from 'react-navigation';
-import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { colors } from 'styles';
+import repositoryService from 'services/repositoryService';
+import api from 'services/api';
 import styles from './styles';
 
 export default class Header extends Component {
-  static propTypes = {
-    navigation: PropTypes.shape({
-      dispatch: PropTypes.func,
-    }).isRequired,
-  };
 
   state = {
-    username: '',
+    repositoryName: '',
   };
 
   addRepository = async () => {
+    if (this.state.repositoryName.length === 0) return;
 
+    const response = await api.get(`/repos/${this.state.repositoryName}`);
+    if (!response.ok) throw Error();
+
+    // TODO filtrar attributes
+    const repository = response.data;
+
+    repositoryService.save(repository).then(result => console.tron.log(result));
   };
 
   render() {
@@ -36,7 +37,7 @@ export default class Header extends Component {
           autoCorrect={false}
           style={styles.input}
           placeholder="Adicionar repositório"
-          onChangeText={(username) => { this.setState({ username }); }}
+          onChangeText={(repositoryName) => { this.setState({ repositoryName }); }}
         />
         <View style={styles.addButtonContainer}>
           <TouchableOpacity onPress={this.addRepository}>
